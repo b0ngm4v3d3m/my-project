08a5194dfe804d087abd92ba2bb5ff0ea976cefc
SINH <b0ngm4v3d3m@gmail.com>
2021-11-16 19:42:07 +0700

    XongChap8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_114641) do
+ActiveRecord::Schema.define(version: 2021_11_16_114130) do
 
   create_table "controllers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "posts"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_114641) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
