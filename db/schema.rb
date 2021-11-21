fc77556e2cdb48b004777f4d350e0a9e02bf1140
SINH <b0ngm4v3d3m@gmail.com>
2021-11-21 15:46:44 +0700

    xongchap10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_111836) do
+ActiveRecord::Schema.define(version: 2021_11_19_120710) do
 
   create_table "controllers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "posts"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2021_11_19_111836) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
